702c93627bd549f8e40ed7ea313b33d3f06a12fb
Albert <ayanit1@googlemail.com>
2017-04-17 18:45:51 +0100

    Included post and delete feature

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var cors = require("cors");
+var bodyParser = require("body-parser"); //middleware, help parse through data that is posted to api
 var app = express();
 
 var skierTerms = [
@@ -17,9 +18,12 @@ var skierTerms = [
 	}
 ];
 
+// allows us to accept both json and url encoded data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 
 app.use(function(req, res, next) {
-	console.log(`${req.method} request for '${req.url}'`);
+	console.log(`${req.method} request for '${req.url}' - ${JSON.stringify(req.body)}`);
 	next();
 });
 
@@ -30,6 +34,20 @@ app.use(cors());
 
 // api request
 app.get("/dictionary-api", function(req, res){
+	res.json(skierTerms);
+});
+
+// adding receiver for any post requests. Pushes request onto skierTerms
+app.post("/dictionary-api", function(req, res){
+	skierTerms.push(req.body);
+	res.json(skierTerms);
+});
+
+// allows us to delete terms by creating delete route
+app.delete("/dictionary-api/:term", function(req, res){
+	skierTerms = skierTerms.filter(function(definition){ //reset skierTerms
+		return definition.term.toLowerCase() !== req.params.term.toLowerCase(); //predicate, returns true means definition stays in array else deletes
+	});
 	res.json(skierTerms);
 });
 
